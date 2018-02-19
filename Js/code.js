71783146dd380f4f6c7c08ea7bfb68862043dc6d
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2018-02-19 23:38:03 +0000

    working basic cal

--- Js/code.js
+++ Js/code.js
@@ -2,55 +2,51 @@
 
 var calculator_choice;
 var selected_choice;
+var isRunning = true;
 
 
-//calculator selection//
-
-alert ("Which calculator would you like to use ?");
+//calculator selection - Basic //
 
 
-var calculator_choice = prompt("(B)Basic (A)Advanced");
+while (isRunning) {
 
 
+    var calculator_choice = prompt("Which calculator would you like to use ? (B)Basic (A)Advanced");
 
-if (calculator_choice == "b"){
+    if (calculator_choice == "b"){
+      var operation = prompt("0. end the program\n1. additions\n2.subtractions\n3. mutiplications\n4.divisions\nType your selection here");
 
-   alert("this calculator only calculates additions, mutiplications, divisions and subtractions");
+      if (operation == 0){
+        alert("program will now end!");
+        isRunning = false;
+      
+      } else if (operation == 1) {
+        var num1 = prompt("Enter the first number");
+        var num2 = prompt("Enter the second number");
+        alert ("The sum is " + (Number(num1)+Number(num2)));
 
-} else if (calculator_choice == "a"){
+      } else if (operation == 2) {
+        var num1 = prompt("Enter the first number");
+        var num2 = prompt("Enter the second number");
+        alert ("The difference is " + (Number(num1)-Number(num2)));
 
-   alert (" This calculator only calculates power and square root")
+      } else if (operation == 3) {
+        var num1 = prompt("Enter the first number");
+        var num2 = prompt("Enter the second number");
+        alert ("The product is " + (Number(num1)*Number(num2)));
+
+      } else if (operation == 4) {
+        var num1 = prompt("Enter the first number");
+        var num2 = prompt("Enter the second number");
+        alert ("The quotiant is " + (Number(num1)/Number(num2)));
+
+      }
+
+    } else if (calculator_choice == "a"){
+     var selected_choice = prompt ("This calculator only calculates power and square root");
+ }
 }
 
-
-// selected calculator //
-
-var operation = ["0. end the program",  "1. additions",  "2.subtractions", "3. mutiplications" ,"4.divisions", "Type your selection here"];
-
-if operation == 0{
-	alert("program will now end!");
-
-} else if (operation == 1) {
-var num1 = prompt("Enter the first number");
-var num2 = prompt("Enter the second number");
-alert ("The sum is " + (Number(num1)+Number(num2)));
-
-} else if (operation == 2) {
-var num1 = prompt("Enter the first number");
-var num2 = prompt("Enter the second number");
-alert ("The difference is " + (Number(num1)-Number(num2)));
-
-} else if (operation == 3) {
-var num1 = prompt("Enter the first number");
-var num2 = prompt("Enter the second number");
-alert ("The product is " + (Number(num1)*Number(num2)));
-
-} else if (operation == 4) {
-var num1 = prompt("Enter the first number");
-var num2 = prompt("Enter the second number");
-alert ("The quotiant is " + (Number(num1)/Number(num2)));
-
-}	
 
 
 
